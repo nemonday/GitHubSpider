99fbd838f48e7e440e47fca5eda639ba44690707
nemo <569857936@qq.com>
2019-05-27 16:10:29 +0800

    1

--- 1.py
+++ 1.py
@@ -1,21 +1,20 @@
 import requests
 import json
 
-for i in range(1):
-    headers = {
-        "Content-type ": "application/json; charset=UTF-8"
-    }
+headers = {
+    "Content-type ": "application/json; charset=UTF-8"
+}
 
-    url = "http://127.0.0.1:8000"
+url = "http://127.0.0.1:8000"
 
-    data = {
-        "call": "test:Transcoding:transcoding",
-        "params": {
-        "path": "video",
-        "osskey": "pythontest{}".format(i+1)
-      }
-    }
+data = {
+    "call": "test:Transcoding:transcoding",
+    "params": {
+    "path": "video",
+    "osskey": "pythontest{}".format(11)
+  }
+}
 
-    data = requests.post(url, headers=headers, data=json.dumps(data))
+data = requests.post(url, headers=headers, data=json.dumps(data))
 
-    print(data.text)
+print(data.text)
